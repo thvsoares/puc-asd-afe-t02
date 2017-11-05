4ac774da78b1ed114296abf59f36c4326219319e
Thiago Soares <thiagomullet@gmail.com>
2017-11-05 15:24:44 -0200

    OAuth update

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var url = require('url');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    }  
-    res.writeHead(200);
-    res.write(data);
-    return res.end();
-  });
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Access-Control-Allow-Origin': 'http://localhost:8080' });
+        res.write(data);
+        return res.end();
+    });
 }).listen(8080);
